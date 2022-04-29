0c03e18341edb7ca9725e8a5c708d4fe421a2264
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-04-29 14:18:52 +0100

    Lab 4

---
+++ src/main/java/com/sip/ams/DevopsGkLab4Application.java
@@ -0,0 +1,14 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGkLab4Application {
+
+	public static void main(String[] args) {
+		System.out.println("Merci Devops");
+		SpringApplication.run(DevopsGkLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sip/ams/DevopsGkLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGkLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
